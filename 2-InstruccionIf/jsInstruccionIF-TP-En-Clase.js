4d4f3fcb6a4aa3df3f8a9fec157ed61a1b46b738
LucianoCrocco <lucianocrocco@outlook.com>
2020-07-23 17:08:09 -0300

    Clase 23/07

--- 2-InstruccionIf/jsInstruccionIF-TP-En-Clase.js
+++ 2-InstruccionIf/jsInstruccionIF-TP-En-Clase.js
@@ -3,7 +3,7 @@ function mostrar()
 /* Calcular dos numeros random entre 1 y 51:
 a)Si los dos son pares: Multiplicarlos. Mostrar dato.
 b)Si los dos son impares: Restarlos. Mostrar dato.
-c)Si alguno es divisible por 5 elevarlo al cubo. Mostrar dato.
+c)Si alguno es divisible por 5 elevarlo al cubo. Mostrar dato. Elevar uno solo
 d)Si no ocurrio ningun caso anterior mostrar "No paso nada"
 e)Mostrar el resultado en un solo alert.
 */
@@ -16,12 +16,6 @@ var numeroDos;
 var alerta;
 var alertaResultadoUno;
 var alertaResultadoDos;
-var alertaResultadoTres;
-var alertaResultadoCuatro;
-var suma1;
-var suma2;
-var suma3;
-var suma4;
 
 max = 51
 min = 1
@@ -39,50 +33,19 @@ if((numeroUno % 2) == 0 && (numeroDos % 2) == 0){
 	console.log((alertaResultadoUno = (numeroUno * numeroDos)) + " Ambos multiplicados");
 } else {
 	if((numeroUno % 2) != 0 && (numeroDos % 2) != 0){
-		console.log(("Ambos son impares ") + (alertaResultadoDos = (numeroUno - numeroDos)));
+		console.log(("Ambos son impares ") + (alertaResultadoUno = (numeroUno - numeroDos)));
 	}
 }
 if((numeroUno % 5) == 0){
-	console.log("El numero uno divisible elevado a la quinta es: " + (alertaResultadoTres = Math.pow(numeroUno, 3)));
-}
-if((numeroDos % 5) == 0){
-	console.log("El numero dos divisible elevado a la quinta es: " + (alertaResultadoCuatro = Math.pow(numeroDos, 3)));
-}
+		console.log("El numero uno divisible elevado a la quinta es: " + (alertaResultadoDos = Math.pow(numeroUno, 3)));
+	} else {
+		if((numeroDos % 5) == 0){
+		console.log("El numero dos divisible elevado a la quinta es: " + (alertaResultadoDos = Math.pow(numeroDos, 3)));
+		}
+	}
 
-if (!((numeroUno % 2) == 0 && (numeroDos % 2) == 0) && (!((numeroUno % 2) != 0 && (numeroDos % 2) != 0))
-&& (!((numeroUno % 5) == 0)) && (!((numeroDos % 5) == 0))){
+if (alertaResultadoUno == 0 && alertaResultadoDos == 0){
 	console.log("No pasa nada.");
 	}
-
-if((numeroUno % 2) == 0 && (numeroDos % 2) == 0){
-		suma1 = alertaResultadoUno;
-} else {
-	suma1 = 0;
-}
-
-if((numeroUno % 2) != 0 && (numeroDos % 2) != 0){
-			suma2 = alertaResultadoDos;
-	} else {
-		suma2 = 0;
-	}
-
-if((numeroUno % 5) == 0){
-	suma3 =	alertaResultadoTres;
-} else {
-	suma3 = 0;
-}
-
-if((numeroDos % 5) == 0){
-	suma4 = alertaResultadoCuatro;
-} else {
-	suma4 = 0;
-}
-
-alerta = suma1 + suma2 + suma3 + suma4;
-
-if (alerta != 0){
-alert ("El resultado cuando se cumple alguna de las condiciones es: " + alerta);
-}
- 
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -7,8 +7,142 @@ D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es de
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
- */
+ */ /*
 function CalcularPrecio () 
 {
- 	
+    var precioIndividual;
+    var cantidadDeLamparitas;
+    var marcaArgentinaLuz;
+    var marcaFelipeLamparas;
+    var marcaJeLuz;
+    var marcaHazIluminacion;
+    var marcaOsram;
+    var precioFinal;
+    var precioImpuesto;
+    var precioDescuento;
+
+    precioIndividual = 35;
+
+    marcaArgentinaLuz = (Marca.value == "ArgentinaLuz");
+    marcaFelipeLamparas = (Marca.value == "FelipeLamparas");
+    marcaJeLuz = (Marca.value == "JeLuz");
+    marcaHazIluminacion = (Marca.value == "HazIluminacion");
+    marcaOsram = (Marca.value == "Osram");
+
+    cantidadDeLamparitas = txtIdCantidad.value;
+    cantidadDeLamparitas = parseInt(cantidadDeLamparitas);
+
+    if (cantidadDeLamparitas >= 6){
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.5;
+        console.log(precioFinal);
+    } else {
+        if(cantidadDeLamparitas == 5 && marcaArgentinaLuz){
+            precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.4;
+            console.log(precioFinal);
+        } else {
+            if (cantidadDeLamparitas == 5){
+                precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.3;
+                console.log(precioFinal);
+            } else {
+                if (cantidadDeLamparitas == 4 && (marcaArgentinaLuz || marcaFelipeLamparas)){
+                    precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.25;
+                    console.log(precioFinal);
+                } else {
+                    if(cantidadDeLamparitas == 4){
+                        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.2;
+                        console.log(precioFinal);
+                    } else{
+                        if(cantidadDeLamparitas == 3 && marcaArgentinaLuz){
+                            precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.15;
+                            console.log(precioFinal);
+                        } else {
+                            if (cantidadDeLamparitas == 3 && marcaFelipeLamparas){
+                                precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.10;
+                                console.log(precioFinal) 
+                            } else {
+                                if(cantidadDeLamparitas == 3) {
+                                precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.05;
+                                console.log(precioFinal);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if(precioFinal >= 120){
+        console.log("Recargo 10%, IIBB usted pago: " + (precioImpuesto = (precioFinal * 1.10) - precioFinal) + " como monto de recargo.")
+        console.log(Math.floor(precioDescuento = (precioImpuesto + precioFinal)))
+        txtIdprecioDescuento.value = parseInt(precioDescuento);;
+    } else {
+        console.log (precioDescuento = precioFinal);
+        txtIdprecioDescuento.value = parseInt(precioDescuento);
+    }
+} */
+/* ESTRUCTURA ELSE-IF
+if(condition){
+    alert(X)
+} else if (condition) {
+    alert (Y)
+}
+*/
+// HECHO CON ELSE IF, NO SE IDENTAN  COMO ARRIBA.
+function CalcularPrecio () 
+{
+    var precioIndividual;
+    var cantidadDeLamparitas;
+    var marcaArgentinaLuz;
+    var marcaFelipeLamparas;
+    var marcaJeLuz;
+    var marcaHazIluminacion;
+    var marcaOsram;
+    var precioFinal;
+    var precioImpuesto;
+    var precioDescuento;
+
+    precioIndividual = 35;
+
+    marcaArgentinaLuz = (Marca.value == "ArgentinaLuz");
+    marcaFelipeLamparas = (Marca.value == "FelipeLamparas");
+    marcaJeLuz = (Marca.value == "JeLuz");
+    marcaHazIluminacion = (Marca.value == "HazIluminacion");
+    marcaOsram = (Marca.value == "Osram");
+
+    cantidadDeLamparitas = txtIdCantidad.value;
+    cantidadDeLamparitas = parseInt(cantidadDeLamparitas);
+
+    if (cantidadDeLamparitas >= 6){
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.5;
+        console.log(precioFinal);
+    } else if(cantidadDeLamparitas == 5 && marcaArgentinaLuz){
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.4;
+        console.log(precioFinal);
+    } else  if (cantidadDeLamparitas == 5){
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.3;
+        console.log(precioFinal); 
+    } else if (cantidadDeLamparitas == 4 && (marcaArgentinaLuz || marcaFelipeLamparas)){
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.25;
+        console.log(precioFinal);
+    } else if(cantidadDeLamparitas == 4){
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.2;
+        console.log(precioFinal);
+    } else if(cantidadDeLamparitas == 3 && marcaArgentinaLuz){
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.15;
+        console.log(precioFinal);
+    } else if (cantidadDeLamparitas == 3 && marcaFelipeLamparas){
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.10;
+        console.log(precioFinal) 
+    } else if(cantidadDeLamparitas == 3) {
+        precioFinal = (precioIndividual * cantidadDeLamparitas) * 0.05;
+        console.log(precioFinal);
+    }
+    if(precioFinal >= 120){
+        console.log("Recargo 10%, IIBB usted pago: " + (precioImpuesto = (precioFinal * 1.10) - precioFinal) + " como monto de recargo.")
+        console.log(Math.floor(precioDescuento = (precioImpuesto + precioFinal)));
+        txtIdprecioDescuento.value = parseInt(precioDescuento);
+        } else {
+            console.log (precioDescuento = precioFinal);
+            txtIdprecioDescuento.value = parseInt(precioDescuento);
+                }
 }
